9200ef1ed3a3c0eb2a134a6c22b34016ddf9f180
max jn <maxjn763@gmail.com>
2023-07-06 16:39:21 +0330

    Movie List & Movie Card Components: - Added **getMovies** to fetch movies server-side - Added **MovieList** component - Added **MovieCard** component with really cool hover effects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,16 @@ import { redirect } from "next/navigation";
 
 //Actions
 import getCurrentUser from "@/actions/getCurrentUser";
+import getMovies from "@/actions/getMovies";
 
 // Components
-import IndexClient from "./IndexClient";
-import Billboard from "@/components/common/layout/Billboard";
+import Billboard from "@/components/common/Billboard";
 import NavBar from "@/components/common/layout/header/NavBar";
+import MovieList from "@/components/movies/MovieList";
 
 export default async function Home() {
   const currentUser = await getCurrentUser();
+  const movies = await getMovies();
 
   //if user is not logged in
   if (!currentUser) {
@@ -21,6 +23,9 @@ export default async function Home() {
     <>
       <NavBar currentUser={currentUser} />
       <Billboard />
+      <section className="pb-40">
+        <MovieList title="Trending Movies" movies={movies} />
+      </section>
     </>
   );
 }
---
+++ src/components/movies/MovieCard.tsx
@@ -0,0 +1,72 @@
+"use client";
+// Hooks / Packages
+import Image from "next/image";
+
+// Types
+import { MovieCardProps } from "@/types/props";
+
+// Iconst
+import { BsFillPlayFill } from "react-icons/bs";
+
+const MovieCard = ({ movie }: MovieCardProps) => {
+  return (
+    <article
+      className="group bg-zinc-900
+   col-span-1 relative h-[12vw]"
+    >
+      {/* Visible Image */}
+      <figure className="h-full w-full cursor-pointer transition duration delay-300 shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 overflow-hidden">
+        <Image
+          src={movie.thumbnailUrl}
+          alt="Thumbnail"
+          fill
+          className="!static object-cover"
+        />
+      </figure>
+      {/* Hover Image */}
+      <div className="opacity-0 absolute top-0 transition duration-200 z-10 invisible sm:visible delay-300 w-full scale-0 group-hover:scale-110 group-hover:-translate-y-[6vw] group-hover:translate-x-[2vw] group-hover:opacity-100">
+        <figure
+          className="cursor-pointer
+          object-cover
+          transition
+          duration
+          shadow-xl
+          rounded-t-md
+          w-full
+          h-[12vw]
+          overflow-hidden"
+        >
+          <Image
+            src={movie.thumbnailUrl}
+            alt="Thumbnail"
+            fill
+            className="!static object-cover"
+          />
+        </figure>
+        <main className="z-10 bg-zinc-800 p-2 lg:p-4 absolute  transition w-full shadow-md rounded-b-md">
+          <div className="flex flex-row items-center gap-3">
+            <button
+              className="w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300"
+              onClick={() => {}}
+            >
+              <BsFillPlayFill size={30} />
+            </button>
+          </div>
+          <p className="text-green-400 font-semibold mt-4">
+            New <span className="text-white">{new Date().getFullYear()}</span>
+          </p>
+          <div className="flex flex-row mt-4 gap-2 items-center">
+            <p className="text-white text-[10px] lg:text-sm">
+              {movie.duration}
+            </p>
+          </div>
+          <div className="flex flex-row mt-4 gap-2 items-center">
+            <p className="text-white text-[10px] lg:text-sm">{movie.genre}</p>
+          </div>
+        </main>
+      </div>
+    </article>
+  );
+};
+
+export default MovieCard;
